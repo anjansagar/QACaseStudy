029d4ca8c576eb1bb57e0a5928a29f1949e6ec3c
Gudapati, Binnu <binnu.gudapati@sabre.com>
2018-10-09 17:39:34 +0530

    basic test to submit a form using click() method

--- src/test/java/com/qa/casestudy/SubmitFormTest.java
+++ src/test/java/com/qa/casestudy/SubmitFormTest.java
@@ -1,5 +1,53 @@
 package com.qa.casestudy;
 
-public class SubmitFormTest {
+import static org.testng.Assert.assertTrue;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * 
+ * @author Binnu
+ *
+ */
+public class SubmitFormTest 
+{
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://bootsnipp.com/login");
+	}
+
+	@Test
+	public void submitLoginFormUsingClickMethod()
+	{
+		WebElement username = driver.findElement(By.name("email"));
+		username.clear();
+		username.sendKeys("binnujesudasan@gmail.com");
+		WebElement password = driver.findElement(By.name("password"));
+		password.clear();
+		password.sendKeys("HelloWorld");
+		driver.findElement(By.cssSelector("input[value='Login']")).click();
+		String errorMessage  = driver.findElement(By.xpath("//*[@id=\"loginform\"]/fieldset/div[1]/h5")).getText();
+		assertTrue(errorMessage.equals("E-mail or password was incorrect, please try again"));
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
 }
